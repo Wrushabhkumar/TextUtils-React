d663a930d4b1ad1d027ab8baec114356be52ac00
Wrushabhkumar Bangar <wrushabh1100@gmail.com>
2025-08-18 15:56:02 +0530

    added Navbar, TextForm and Dark/Light mode switch.

---
+++ src/Components/TextForm.js
@@ -0,0 +1,69 @@
+
+import React, { useState } from 'react';
+
+export default function TextForm(props) {
+    const [text, setText] = useState('');
+
+    const handleLwClick=()=>{
+        setText(text.toLowerCase());
+    }
+
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+
+    const handleOnChange = (event) => {
+        setText(event.target.value)
+    }
+
+    const handleClearClick=()=>{
+       let newText = '';
+        setText(newText)
+    }
+
+    const handleCopy = ()=>{
+        let text = document.getElementById('myBox')
+        text.select()
+        navigator.clipboard.writeText(text.value)
+    }
+
+    const handleExtraSpaces=()=>{
+        let newText= text.split(/[ ]+/)
+        setText (newText.join(" "))
+    }
+
+    return (
+        <>
+        <div style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
+            <h1>{props.heading}</h1>
+            <div className="mb-3" style={{backgroundColor: props.mode==='light'?'white':'#042743'}}>
+                <textarea className="form-control"
+                style={{backgroundColor: props.mode==='light'?'white':'#042743', color: props.mode === 'light' ? 'black' : 'white'}}
+                    value={text}
+                    onChange={handleOnChange}
+                    id="myBox"
+                    rows="12">
+
+                </textarea>
+                <button className="btn btn-primary mx-1 my-3"onClick={handleUpClick}>Uppercase</button>
+                <button className="btn btn-primary mx-1 my-3"onClick={handleLwClick}>Lowercase</button>
+                <button className="btn btn-primary mx-1 my-3"onClick={handleClearClick}>Cleare Text</button>
+                <button className="btn btn-primary mx-1 my-3"onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-primary mx-1 my-3"onClick={handleExtraSpaces}>Remove Extra spaces</button>
+
+            </div>
+        </div>
+
+        <div className="conatainer" style={{ color: props.mode === 'light' ? 'black' : 'white' }}>
+            <h3>Text Summary</h3>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes to Reade</p>
+
+            <h3>preview</h3>
+            <p>{text.length>0?text:"Enter something in the textbox to preview it here"}</p>
+        </div>
+        </>
+    );
+}
+
